d35283e975be0cf2cd37b8baa70d6f3a3a26b8ed
sivasathvik <paturisivasathvik@gmail.com>
2023-12-11 15:49:01 +0530

    React app file

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Login from './components/login';
+
+//import Login from './Login';
+
+function App() {
+  return (
+    <div className= "App">
+      <h1>React App</h1>
+      <Login></Login>
+      
+    </div>
+  );
+}
+
+export default App;
